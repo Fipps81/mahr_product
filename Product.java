30f3cc6c7200347fddbbc617e85c28dda4c6a8fe
Fipps <stick@chello.at>
2021-03-13 17:15:03 +0100

    Create Product class

---
+++ Product.java
@@ -0,0 +1,18 @@
+public class Product
+{
+	private String name;
+	public Product(String name)
+	{
+		setName(name);
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+
+}
